ed219858c59eb16b9151b6d0cf0fd8c8bfa62eb9
talesam <talesam@gmail.com>
2025-10-09 13:31:25 -0300

    🔨 refactor: Added support for appimage in translation

--- usr/share/ashyterm/utils/translation_utils.py
+++ usr/share/ashyterm/utils/translation_utils.py
@@ -4,9 +4,27 @@
 # translation_utils.py - Utilities for translation support
 #
 import gettext
+import os
+
+# Determine locale directory (works in AppImage and system install)
+locale_dir = '/usr/share/locale'  # Default for system install
+
+# Check if we're in an AppImage
+if 'APPIMAGE' in os.environ or 'APPDIR' in os.environ:
+    # Running from AppImage
+    # translation_utils.py is in: usr/share/ashyterm/utils/translation_utils.py
+    # We need to get to: usr/share/locale
+    script_dir = os.path.dirname(os.path.abspath(__file__))  # usr/share/ashyterm/utils
+    app_dir = os.path.dirname(script_dir)                    # usr/share/ashyterm
+    share_dir = os.path.dirname(app_dir)                     # usr/share
+    appimage_locale = os.path.join(share_dir, 'locale')      # usr/share/locale
+    
+    if os.path.isdir(appimage_locale):
+        locale_dir = appimage_locale
 
 # Configure the translation text domain for comm-ashyterm
-gettext.textdomain("ashyterm")
+gettext.bindtextdomain("comm-ashyterm", locale_dir)
+gettext.textdomain("comm-ashyterm")
 
 # Export _ directly as the translation function
 _ = gettext.gettext
