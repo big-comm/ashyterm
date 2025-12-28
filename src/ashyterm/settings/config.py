85b3921abe49b4dd3eafda878cead891018f9b60
talesam <talesam@gmail.com>
2025-12-27 21:22:30 -0300

    💎 style: Updated description of PKGBUILD

--- src/ashyterm/settings/config.py
+++ src/ashyterm/settings/config.py
@@ -32,7 +32,7 @@ class AppConstants:
 
     APP_ID = "org.communitybig.ashyterm"
     APP_TITLE = "Ashy Terminal"
-    APP_VERSION = "1.8.4"
+    APP_VERSION = "1.8.5"
     DEVELOPER_NAME = "BigCommunity"
     DEVELOPER_TEAM = ["BigCommunity Team"]
     COPYRIGHT = "© 2025 BigCommunity"
